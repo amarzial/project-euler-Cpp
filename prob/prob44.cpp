e1df78ce9655332ee1293659b1a88ee3e04e83e2
Alessandro Marzialetti <setti93@gmail.com>
2017-07-20 19:16:17 +0200

    Improved algorithm for problem 44

--- prob/prob44.cpp
+++ prob/prob44.cpp
@@ -1,8 +1,15 @@
 #include <cmath>
+#include <cstdint>
 #include <iostream>
 #include <unordered_set>
 
 namespace prob44 {
+
+bool is_pentagonal(int n) {
+  int num = (1 + sqrt(1 + 24 * n)) / 6;
+  return (num * (3 * num - 1) / 2) == n;
+}
+
 int Start() {
   std::unordered_set<int> pents;
   int n = 2;
@@ -10,6 +17,8 @@ int Start() {
     ++n;
     int s = n * (3 * n - 1) / 2;
     for (auto pj : pents) {
+      if (not is_pentagonal(s - pj) or not is_pentagonal(s - (2 * pj)))
+        continue;
       if (pents.find(s - pj) != pents.end() and
           pents.find((s - pj) - pj) != pents.end()) {
         std::cout << ((s - pj) - pj) << std::endl;
