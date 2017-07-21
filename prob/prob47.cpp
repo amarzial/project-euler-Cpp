2c096e30344639d6aa0bf19a0e5cef4ec01323e8
Alessandro Marzialetti <setti93@gmail.com>
2017-07-21 18:42:42 +0200

    Problem 47 speed up x30

--- prob/prob47.cpp
+++ prob/prob47.cpp
@@ -5,7 +5,7 @@
 
 namespace prob47 {
 
-std::vector<unsigned int> primes{2, 3, 5, 7, 11, 13, 17, 19};
+std::vector<int> primes{2, 3, 5, 7, 11, 13, 17, 19};
 
 int next_prime(int n) {
   if (n == 2) return 3;
@@ -15,37 +15,51 @@ int next_prime(int n) {
   return (n);
 }
 
-bool has_four_prime_factors(unsigned int n) {
-  unsigned int prime;
+bool has_four_prime_factors(int n) {
+  if (utils::is_prime_miller(n)) return false;
+  int prime;
+  auto rem = n;
   int factors = 0;
   for (auto p : primes) {
     prime = p;
-    if (n % prime == 0) ++factors;
-    while (n % prime == 0) n /= prime;
-    if (n <= 1) break;
+    if (rem % prime == 0) ++factors;
+    while (rem % prime == 0) {
+      rem /= prime;
+      if (utils::is_prime_miller(rem)) {
+        ++factors;
+        rem = 1;
+        break;
+      }
+    }
+    if (rem <= 1) break;
   }
-  while (n > 1) {
+  while (rem > 1) {
     prime = next_prime(prime);
     primes.push_back(prime);
-    if (n % prime == 0) ++factors;
-    while (n % prime == 0) n /= prime;
+    if (rem % prime == 0) ++factors;
+    while (rem % prime == 0) {
+      rem /= prime;
+      if (utils::is_prime_miller(rem)) {
+        ++factors;
+        rem = 1;
+        break;
+      }
+    }
   }
   return factors == 4;
 }
 
 int Start() {
-  unsigned int i = 647;
-  while (true) {
-    int cnt = 0;
-    while (has_four_prime_factors(i + cnt)) {
-      if (cnt == 3) {
-        std::cout << i << std::endl;
-        return 0;
-      }
+  int i = 647;
+  int cnt = 0;
+  while (cnt < 4) {
+    if (has_four_prime_factors(i++)) {
       ++cnt;
+    } else {
+      cnt = 0;
     }
-    i += 1 + cnt;
   }
-  return 1;
+  std::cout << i << std::endl;
+  return 0;
 }
 }
